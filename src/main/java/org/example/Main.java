ddce8023f746d670cbe9685860831c542c73f923
ryuj7338 <ryuj7338@gmail.com>
2025-03-05 16:42:52 +0900

    0305수업1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 
 package org.example;
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" 1개 입력 : ");
+
+        String num = sc.nextLine();
+
+        System.out.print("num : " +num);
+
         // %c : 문자 서식 지정자
         // %d : 정수(10진수) 서식 지정자
         // %f : 실수
