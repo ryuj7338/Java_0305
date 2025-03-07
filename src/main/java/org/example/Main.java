db8febca2da54ebca3472a622064c5aea50a0394
ryuj7338 <ryuj7338@gmail.com>
2025-03-07 16:45:49 +0900

    0307수업1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-      int[] numArr = new int[3] {10, 20, 30};
+   //   int[] numArr = new int[3] {10, 20, 30};
+      boolean[] arr1 = new boolean[3];
+      arr1[0] = true;
+      arr1[1] = false;
+      arr1[2] = true;
+
+
+//        float[] arr2 = new float[3]{3.14, 7.77, 11.11};
+//        int[] arr3 = new int[11];
+
+        System.out.println();
 
 
 
