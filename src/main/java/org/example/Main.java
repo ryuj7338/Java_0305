75e2ba9a2494b9a4a9da499f36d61cd14e7e3672
ryuj7338 <ryuj7338@gmail.com>
2025-03-07 16:12:08 +0900

    0307수업

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      int[] numArr = new int[3] {10, 20, 30};
+
+
+
+    }
+}
+
+
+
+
