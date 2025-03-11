2585cd3cd9d4c2eac1ffe506492ba0b54265d83b
ryuj7338 <ryuj7338@gmail.com>
2025-03-11 16:49:50 +0900

    03자바수업 과제

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,66 @@
 package org.example;
 
-// <눈에 익혀 두기>
-// void는 retrun 안 할 의무가 있다. void가 없는 경우는 return을 꼭 해야 한다.
-// 전역변수 : 전역 공간(함수 외)에서 만든 변수
-// 지역변수 : 지역 공간(함수 내)에서 만든 변수
-// 매개변수 parameter : 매개체
-// 인자, 인수, args : 함수 실행할 때 옵션
-// static : 객체화 ㄴㄴ
-// ----------------------------------------------------
-// 알아둘 키워드 : 리턴 타입, 리턴, 리턴 밸류
-// 문제 : 아래와 같이 출력 되도록 해주세요.
+// 1부터 n까지 소수의 갯수를 출력하는 함수 만들기
 
 class Main {
     public static void main(String[] args) {
-        int 결과1 = Math.nToMSum(2, 3);
-        System.out.println("결과1 : " + 결과1);
-        // 출력 : 결과1 : 5
+        boolean result = Math.isPrimeNumber(1);
+        System.out.println("1은(는) 소수인가? : " + result);
+        // false
+        result = Math.isPrimeNumber(2);
+        System.out.println("2은(는) 소수인가? : " + result);
+        // true
+        result = Math.isPrimeNumber(3);
+        System.out.println("3은(는) 소수인가? : " + result);
+        // true
+        result = Math.isPrimeNumber(4);
+        System.out.println("4은(는) 소수인가? : " + result);
+        // false
+//        int rs = one_to_n_prime_numbers_count(10);
+//        System.out.println("rs : " + rs);
+//        // rs : 4
+//        rs = one_to_n_prime_numbers_count(13);
+//        System.out.println("rs : " + rs);
+//        // rs :  6
 
-        int 결과2 = Math.nToMSum(5, 10);
-        System.out.println("결과2 : " + 결과2);
-        // 출력 : 결과2 : 45
+//        boolean rs = Math2.isEvenNumber(2);
+//        System.out.println(rs);
+//        // true
+//
+//        rs = Math2.isEvenNumber(3);
+//        System.out.println(rs);
+//        // false
+
+
     }
 }
 
 class Math {
 
-    public static int nToMSum(int n, int m) {
-        int sum = 0;
 
+    public static boolean isPrimeNumber(int n) {
 
+       if(1 < n) {
+           if (n % )
+       }
     }
 }
 
+class one_to_n_prime_numbers_count {
+
+}
+
+//class Math2 {
+//
+//    public static boolean isEvenNumber(int i) {
+//          return i % 2 == 0;
+//
+//
+//        if (i % 2 == 0) {
+//            return true;
+//           }
+//           return false;
+//        }
+//
+//    }
+//}
