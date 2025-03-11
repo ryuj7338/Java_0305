9a1e84283766f96e1899b4dff1094e197b88e4d7
ryuj7338 <ryuj7338@gmail.com>
2025-03-11 11:44:51 +0900

    0311수업2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 package org.example;
 
+// <눈에 익혀 두기>
+// void는 retrun 안 할 의무가 있다. void가 없는 경우는 return을 꼭 해야 한다.
+// 전역변수 : 전역 공간(함수 외)에서 만든 변수
+// 지역변수 : 지역 공간(함수 내)에서 만든 변수
+// 매개변수 parameter : 매개체
+// 인자, 인수, args : 함수 실행할 때 옵션
+// static : 객체화 ㄴㄴ
+// ----------------------------------------------------
+// 알아둘 키워드 : 리턴 타입, 리턴, 리턴 밸류
 
-
-
-public class Main {
-
-    int a =20;  // 전역 변수
-
-
+class Main {
     public static void main(String[] args) {
+        계산기.합(a: 10, b: 20);
 
-        int a = 30;
-
-        계산기.합(a: 10, b: 20)
 
 
     }
@@ -20,14 +21,11 @@ public class Main {
 }
 
 class 계산기 {
-    int a = 40;
-
-    static void 합(int a, int b) {
-//        int a =50;  // 지역 변수
-
+    static int 합(int a, int b) {
+//
         System.out.println("a : " +a);
         System.out.println("b : " +b);
 
-        System.out.println(a + b);
+        System.out.println("a + b : "+(a + b));
     }
 }
